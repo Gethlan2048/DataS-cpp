cc29c8736b6b9728415a5eb221e177cb98ff0dee
Gethlan2048 <geth2048@gmail.com>
2022-10-26 22:34:56 -0500

    Add to sorted to test sortedtype template.

--- sorted.cpp
+++ sorted.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
-#include"ItemType.h"
-#include"sortedlinkedlist.h"
+#include<string.h>
+#include"sortedlinkedlist_t.h"
 int main()
 {
-    SortedType list1;
-    ItemType one;
-    ItemType two;
-
-    one.Initilize(1);
-    two.Initilize(2);
-    list1.PutItem(one);
-    list1.PutItem(two);
-    list1.GetItem(one).Print();
-    list1.GetItem(two).Print();
-    list1.GetItem(one).Print();
-    list1.DeleteItem(one);
+    using namespace std;
+    SortedType<int> list1;
+    SortedType<string> list2;
+ 
+    list2.PutItem("Blaine");
+    cout<< list2.GetItem("Blaine");
+    list1.PutItem(121212);
+    list1.PutItem(32);
+    list1.GetItem(121212);
+    cout<<list1.GetItem(32) << endl;
+    list1.DeleteItem(32);
+    std::cout<<list1.GetItem(121212)<<std::endl;
 }
